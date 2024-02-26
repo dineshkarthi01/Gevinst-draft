4983ff2eb1941df5d797ba818daa73bc1b150f8c
dineshkarthi01 <131676023+dineshkarthi01@users.noreply.github.com>
2024-02-27 02:58:03 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import TextEditor from './components/TextEditor';
+
+
+const App = () => {
+  return (
+    <div>
+      <h1>My Text Editor</h1>
+      <TextEditor />
+    </div>
+  );
+};
+
+export default App;
